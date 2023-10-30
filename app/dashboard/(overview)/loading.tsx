dda86dfc0b425408e8d72630f6413c058ed7894c
Bashir Aziz <bashiraziz@gmail.com>
2023-10-29 22:50:41 -0400

    Second

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,10 +1,5 @@
-import DashboardSkeleton from "../../ui/skeletons"
-
-export default function Loading(){
-    return(
-        <div>
-            <h1>Loading....</h1>
-            <DashboardSkeleton />
-        </div>
-    )
+import DashboardSkeleton from '@/app/ui/skeletons';
+ 
+export default function Loading() {
+  return <DashboardSkeleton />;
 }
