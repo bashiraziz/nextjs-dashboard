972dbc09c955a0ee0e55bc7a8d67bc8b7bf36620
Bashir Aziz <bashiraziz@gmail.com>
2023-10-29 22:07:17 -0400

    gitignore modified

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,10 @@
+import DashboardSkeleton from "../../ui/skeletons"
+
+export default function Loading(){
+    return(
+        <div>
+            <h1>Loading....</h1>
+            <DashboardSkeleton />
+        </div>
+    )
+}
