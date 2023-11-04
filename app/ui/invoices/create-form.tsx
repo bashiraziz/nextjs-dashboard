93c2e3a7f0dceb5d82e057b743bd1023b348498a
Bashir Aziz <bashiraziz@gmail.com>
2023-11-03 20:56:25 -0400

    error checking fixed and running successfully

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -10,12 +10,18 @@ import {
 } from '@heroicons/react/24/outline';
 import { Button } from '../button';
 import { createInvoice } from '@/app/lib/actions';
+import { useFormState } from 'react-dom';
+
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  
+  const initialState = { message: null, errors: {} };
+  const [state, dispatch] = useFormState(createInvoice, initialState);
+  
   return (
-    <form action = {createInvoice}>
+    <form action = {dispatch}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
-        Customer Name
+        {/* Customer Name */}
         <div className="mb-4">
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
             Choose customer
@@ -26,6 +32,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
               name="customerId"
               className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               defaultValue=""
+              aria-describedby="customer-error"
             >
               <option value="" disabled>
                 Select a customer
@@ -38,12 +45,23 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             </select>
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
+          {state.errors?.customerId ? (
+        <div
+          id="customer-error"
+          aria-describedby="customer-error"
+          className="mt-2 text-sm text-red-500"
+        >
+          {state.errors.customerId.map((error: string) => (
+            <p key={error}>{error}</p>
+          ))}
+        </div>
+      ) : null}
         </div>
 
         Invoice Amount
         <div className="mb-4">
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
-            Choose an amount
+            {/* Choose an amount */}
           </label>
           <div className="relative mt-2 rounded-md">
             <div className="relative">
@@ -53,27 +71,46 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 type="number"
                 step="0.01"
                 placeholder="Enter USD amount"
+                aria-describedby="customer-error"
                 className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
+            {state.errors?.amount? (
+        <div
+          id="amount-error"
+          aria-live="polite"
+          className="mt-2 text-sm text-red-500"
+        >
+          {state.errors.amount.map((error: string) => (
+            <p key={error}>{error}</p>
+          ))}
+        </div>
+      ) : null}
           </div>
-          s
+          
         </div>
 
-        Invoice Status
+        {/* Invoice Status */}
         <div>
           <label htmlFor="status" className="mb-2 block text-sm font-medium">
             Set the invoice status
           </label>
-          <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
-            <div className="flex gap-4">
-              <div className="flex items-center">
+          <
+		  
+		  div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
+            
+			
+			<div className="flex gap-4">
+              
+			  
+			  <div className="flex items-center">
                 <input
                   id="pending"
                   name="status"
                   type="radio"
                   value="pending"
+                  aria-describedby="customer-error"
                   className="h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600"
                 />
                 <label
@@ -82,13 +119,15 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 >
                   Pending <ClockIcon className="h-4 w-4" />
                 </label>
-              </div>
-              <div className="flex items-center">
+				</div>
+              
+			  <div className="flex items-center">
                 <input
                   id="paid"
                   name="status"
                   type="radio"
                   value="paid"
+                  aria-describedby="customer-error"
                   className="h-4 w-4 border-gray-300 bg-gray-100 text-gray-600 focus:ring-2 focus:ring-gray-500 dark:border-gray-600 dark:bg-gray-700 dark:ring-offset-gray-800 dark:focus:ring-gray-600"
                 />
                 <label
@@ -97,10 +136,25 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 >
                   Paid <CheckIcon className="h-4 w-4" />
                 </label>
-              </div>
-            </div>
-          </div>
+			</div>
+			
+			
+			</div>
+		  
+		  </div>
+          
+          {state.errors?.status? (
+        <div
+          id="status-error"
+          aria-live="polite"
+          className="mt-2 text-sm text-red-500"
+        >
+          {state.errors.status.map((error: string) => (
+            <p key={error}>{error}</p>
+          ))}
         </div>
+      ) : null}
+              
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
@@ -110,6 +164,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
           Cancel
         </Link>
         <Button type="submit">Create Invoice</Button>
+      </div>
       </div>
     </form>
   );
